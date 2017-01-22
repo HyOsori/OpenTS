08c715c2d4f664b963d196cb2e1e1f4f689a7243
HCH <hch@RadioactiveAir.local>
2017-01-22 16:29:56 +0900

    front-end

---
+++ WebServer/web/static/search_view.jsx
@@ -0,0 +1,24 @@
+/**
+ * Created by hch on 2017. 1. 22..
+ */
+
+class View_Search extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (
+            <div className="search_background">
+                <input className="search_bar">
+                    
+                </input>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<View_Search/>, document.getElementById('content'));
--- WebServer/web/views.py
+++ WebServer/web/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 from django.shortcuts import render_to_response
 
-def index(request):
-    return render_to_response('index.html')
+def search(request):
+    return render_to_response('search.html')
