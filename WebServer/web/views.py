a081fbadfc094935084df86d21e37e4e7f174cd5
dylan <dylan@lineable.net>
2017-01-22 16:44:17 +0900

    result

--- WebServer/web/views.py
+++ WebServer/web/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render_to_response
 
 def search(request):
     return render_to_response('search.html')
+
+def result(request):
+    return render_to_response('result.html')
