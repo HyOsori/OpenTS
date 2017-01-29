85b14c714c81dc55f3a18a83cb097e2477390a79
Changmin <lcmini6528@gmail.com>
2017-01-29 22:17:27 +0900

    result position

--- WebServer/web/static/result_view.jsx
+++ WebServer/web/static/result_view.jsx
@@ -2,23 +2,41 @@
  * Created by hch on 2017. 1. 22..
  */
 
-class View_Result extends React.Component
-{
-    constructor(props)
-    {
+class View_Result_header extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    render()
-    {
+    render() {
         return (
-            <div className="result_background">
-                <p className="result_bar">SAMSUNG</p>
-                <p className="result_stock">^1800000</p>
-                <a className="result_detail">6시간 전(2016/01/18 - 13:48:27) 갱신된 자료입니다.</a>
+            <div className="result_header">
+                <div className="container">
+                    <div className="result_stock">^1800000</div>
+                    <div className="result_title">SAMSUNG</div>
+                </div>
             </div>
+
+
         )
     }
 }
 
-ReactDOM.render(<View_Result/>, document.getElementById('content'));
+class View_Result_content extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="result_content">
+                <div className="container">
+                    여기에 내용을 쓰자꾸나
+                </div>
+            </div>
+
+
+        )
+    }
+}
+ReactDOM.render(<View_Result_header/>, document.getElementById('titleBar'));
+ReactDOM.render(<View_Result_content/>, document.getElementById('content'));
