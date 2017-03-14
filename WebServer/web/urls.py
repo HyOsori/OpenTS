31fdc50b8533e0b13300cf84cab758e9b3d07367
905 <rnrudgh@gmail.com>
2017-03-14 14:43:43 +0900

    오타~ㅋ

--- WebServer/web/urls.py
+++ WebServer/web/urls.py
@@ -40,6 +40,6 @@ while XASessionEventHandler.login_state == 0:
 urlpatterns = [
     url(r'^search/', views.search, name='search'),
     url(r'^result/', views.result, name = 'result'),
-    url(r'^crawl/', views.crawl, name='crawl'),
+    url(r'^crawl/', views.Crawling.as_view, name='crawl'),
     url(r'^getshcode/', views.getshcode, name='getshcode')
 ]
